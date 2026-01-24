2d3aacd86647e5ecd95dd04ca63fd843e418c3d9
Sabona <sebonawaktole@gmail.com>
2026-01-24 14:09:59 +0300

    feat: Add theme context and API client, installing necessary dependencies.

---
+++ src/data/users.ts
@@ -0,0 +1,19 @@
+
+export const users = [
+    {
+        id: '1',
+        name: 'Sara Jenkins',
+        email: 'sara@example.com',
+        password: 'password123', // In a real app, this would be hashed!
+        role: 'Admin',
+        avatar: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+    },
+    {
+        id: '2',
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: 'password123',
+        role: 'Editor',
+        avatar: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+    }
+];
